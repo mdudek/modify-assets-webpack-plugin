c0d229aaad5ffc73a67150f7f7349f9acb343b69
Michael Dudek <michael.dudek@centrum.cz>
2020-12-01 16:31:36 +0100

    - updated plugin to support Webpack v5

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const apply = function(options, compiler) {
+const apply = function (options, compiler) {
 	if (options == null) {
 		options = {};
 	}
@@ -8,23 +8,31 @@ const apply = function(options, compiler) {
 	if (test == null) {
 		return;
 	}
-	var callback = options.callback || function (filepath, source) { return source; };
+	var callback = options.callback || function (filepath, source) {
+		return source;
+	};
 	var dirname = options.dirname || path.resolve(__dirname);
-	compiler.plugin('emit', function(compilation, done) {
-		for (var basename in compilation.assets) {
-			var filepath = path.resolve(dirname, basename);
-			if (test.test(filepath)) {
-				var asset = compilation.assets[basename];
-				var origFn = asset.source();
-				asset.source = () => callback(filepath, origFn);
-				asset.size = () => asset.source().length;
-			}
-		}
-		done();
+	compiler.hooks.compilation.tap('ModifyAssetsPlugin', compilation => {
+		compilation.hooks.processAssets.tap(
+			{
+				name: 'ModifyAssetsPlugin',
+				stage: compilation.PROCESS_ASSETS_STAGE_PRE_PROCESS
+			},
+			(assets) => {
+				for (var basename in assets) {
+					var filepath = path.resolve(dirname, basename);
+					if (test.test(filepath)) {
+						var asset = assets[basename];
+						var origFn = asset.source();
+						asset.source = () => callback(filepath, origFn);
+						asset.size = () => asset.source().length;
+					}
+				}
+			});
 	});
 };
 
-module.exports = function(options) {
+module.exports = function (options) {
 	return {
 		apply: apply.bind(this, options)
 	};
